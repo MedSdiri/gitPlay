274369307bda5b73fd39d52e115577731f3667cc
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-02 01:39:35 -0600

    created branch called method1 and a print code in the main method

--- src/mainClass.java
+++ src/mainClass.java
@@ -2,5 +2,7 @@ public class mainClass {
     public static void main(String[] args) {
         System.out.println("this is the main class");
         System.out.println("sharing the source folder");
+        System.out.println("just created a branch method1");
+        MethodOne.method1();
     }
 }
