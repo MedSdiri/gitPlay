407bdc1a76aacf8c8df381164e2ef0f69b6fdb0c
Ahmed Sdiri <sdiri.ad@gmail.com>
2020-12-02 01:37:18 -0600

    trying to share the source folder

---
+++ src/mainClass.java
@@ -0,0 +1,6 @@
+public class mainClass {
+    public static void main(String[] args) {
+        System.out.println("this is the main class");
+        System.out.println("sharing the source folder");
+    }
+}
